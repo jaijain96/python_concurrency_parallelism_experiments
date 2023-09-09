4967c1da243b391b2272a08aef73a03bc238f3c9
jai <jaijain96@gmail.com>
2023-09-10 01:47:59 +0530

    synchronization without locks, using basic time.sleep

--- multithreading_test/lock_sync.py
+++ multithreading_test/lock_sync.py
@@ -6,26 +6,35 @@ from colorama import Back
 
 
 def funct(lock: threading.Lock, pipe: list):
-    print("thread: start")
+    print(f"{Back.CYAN}thread: begin{Back.RESET}")
+    print(
+        f"{Back.CYAN}thread: going to sleep for 0.05 sec to make sure there is no race condition between this thread and main, this is to make sure that main is ahead in its execution, ensuring that main is the producer and this thread is the consumer{Back.RESET}"
+    )
+    begin = time.perf_counter()
+    time.sleep(0.05)  # release execution
+    end = time.perf_counter()
+    print(f"{Back.CYAN}thread: woke up after {end - begin} secs{Back.RESET}")
     for index in range(5):
-        print("thread: acquiring lock")
-        if lock.locked():
-            print(f"{Back.RED}thread: waiting for lock to be released...{Back.RESET}")
-        start = time.perf_counter()
-        lock.acquire()
+        print(
+            f"{Back.CYAN}thread: going to sleep for 0.1 sec to make sure there is no race condition between this thread and main, this will allow main to acquire lock and push something in pipe{Back.RESET}"
+        )
+        begin = time.perf_counter()
+        time.sleep(0.1)  # release execution
         end = time.perf_counter()
-        print(f"{Back.CYAN}thread: acquired lock in: {end - start} seconds{Back.RESET}")
-        print(f"{Back.CYAN}thread: putting index {index} in pipe{Back.RESET}")
-        pipe.append(f"thread: {index}")
-        time.sleep(1)
-        print(f"{Back.CYAN}thread: releasing lock{Back.RESET}")
-        lock.release()
-        time.sleep(0.5)
-    print("thread: ends")
+        print(f"{Back.CYAN}thread: woke up after {end - begin} secs{Back.RESET}")
+        # print(f"{Back.CYAN}thread: attempting to acquire lock{Back.RESET}")
+        # begin = time.perf_counter()
+        # lock.acquire()
+        # end = time.perf_counter()
+        # print(f"{Back.CYAN}thread: acquired lock in: {end - begin} seconds{Back.RESET}")
+        print(f"{Back.GREEN}thread: popping {pipe.pop()} from pipe{Back.RESET}")
+        # print(f"{Back.CYAN}thread: releasing lock{Back.RESET}")
+        # lock.release()
+    print(f"{Back.CYAN}thread: ends{Back.RESET}")
 
 
 if __name__ == "__main__":
-    print("main: start")
+    print(f"{Back.YELLOW}main: begin{Back.RESET}")
     lock = threading.Lock()
     """
     acquiring a lock doesn't mean that execution of other threads
@@ -38,23 +47,29 @@ if __name__ == "__main__":
     thread = threading.Thread(target=funct, name="thread", args=(lock, pipe))
     thread.start()
     """
-    note that when we are starting another thread and we haven't
-    acquired a lock in the current thread, we are essentially inducing
-    a race condition between the current thread and the new thread(s)
-    to acquire the lock
+    note that when we are begining another thread and we haven't acquired a
+    lock in the current thread, we are essentially inducing a race condition 
+    between the current thread and the new thread(s) to acquire the lock;
+    to prevent this, we deliberately suspend the execution of the other thread
+    using time.sleep() so that main thread can proceed to acquire the lock and
+    push in some data into the communication pipe (acting as queue)
     """
-    print("back in main")
+    print(f"{Back.YELLOW}back in main{Back.RESET}")
     for index in range(5):
-        print("main: acquiring lock")
-        if lock.locked():
-            print(f"{Back.RED}main: waiting for lock to be released...{Back.RESET}")
-        start = time.perf_counter()
-        lock.acquire()
+        print(
+            f"{Back.YELLOW}main: going to sleep for 0.1 sec to make sure there is no race condition between main and any other thread, this will allow main to acquire lock and push something in pipe{Back.RESET}"
+        )
+        begin = time.perf_counter()
+        time.sleep(0.1)  # release execution
         end = time.perf_counter()
-        print(f"{Back.YELLOW}main: acquired lock in: {end - start} seconds{Back.RESET}")
-        print(f"{Back.YELLOW}main: putting index {index} in pipe{Back.RESET}")
-        pipe.append(f"main: {index}")
-        time.sleep(1)
+        print(f"{Back.YELLOW}main: woke up after {end - begin} secs{Back.RESET}")
+        # print(f"{Back.YELLOW}main: attempting to acquire lock{Back.RESET}")
+        # begin = time.perf_counter()
+        # lock.acquire()
+        # end = time.perf_counter()
+        # print(f"{Back.YELLOW}main: acquired lock in: {end - begin} seconds{Back.RESET}")
+        print(f"{Back.GREEN}main: putting index {index} in pipe{Back.RESET}")
+        pipe.append(index)
         """
         can we do without the above time.sleep(1)?
         consider what happens if we don't suspend the execution of the
@@ -63,17 +78,16 @@ if __name__ == "__main__":
         using lock.release()
         if we don't have time.sleep(1), the current thread acquires
         the thread and releases very quickly, this would cause a race
-        condition between the current thread and the previous thread
-        or any other thread waiting to acquire the lock
+        condition between the current thread and other thread(s) waiting to
+        acquire the lock
         """
-        print(f"{Back.YELLOW}main: releasing lock{Back.RESET}")
-        lock.release()
-        time.sleep(0.5)
+        # print(f"{Back.YELLOW}main: releasing lock{Back.RESET}")
+        # lock.release()  # release lock
         """
         can we do without the above time.sleep(0.5)?
         when we release the lock, we should give opportunity for
         other threads to acquire the lock, coz the loop in
-        current thread can start it's next iteration super quickly
+        current thread can begin it's next iteration super quickly
         and the other threads don't get a chance to acquire it,
         it is on the whims of the os to suspend control of this thread
         and let the other threads run a few statement so they can
@@ -96,8 +110,7 @@ if __name__ == "__main__":
         is not desirable
         """
     thread.join()
-    print("main: ends")
-    pp(pipe)
+    print(f"{Back.YELLOW}main: ends{Back.RESET}")
     """
     here we are using locks as a synchronization mechanism but
     we also rely on time.sleep() to prevent race conditions, are there
